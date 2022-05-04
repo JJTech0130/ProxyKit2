f166d34a01b6d9b43519bb127d803db21db00d3a
JJTech <jjtech@jjtech.dev>
2022-05-03 22:38:49 -0400

    Basic stuff, verbatim applicationmonitor from Clip

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "ProxyKit2",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        //.product(name: "NIO", package: "swift-nio")
         .library(
             name: "ProxyKit2",
             targets: ["ProxyKit2"]),
@@ -14,13 +15,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ProxyKit2",
-            dependencies: []),
+            dependencies: [.product(name: "NIO", package: "swift-nio")]),
         .testTarget(
             name: "ProxyKit2Tests",
             dependencies: ["ProxyKit2"]),
