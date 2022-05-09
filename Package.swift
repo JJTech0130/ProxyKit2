029613fcfa5acbd99e655b92a94772c3b0a30e37
JJTech <jjtech@jjtech.dev>
2022-05-09 18:45:18 -0400

    Add example

--- Package.swift
+++ Package.swift
@@ -16,13 +16,19 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ProxyKit2",
-            dependencies: [.product(name: "NIO", package: "swift-nio"), .product(name: "NIOHTTP1", package: "swift-nio")]),
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+            ]),
         .testTarget(
             name: "ProxyKit2Tests",
             dependencies: ["ProxyKit2"]),
